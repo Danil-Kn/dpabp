d2728c5bf3baba89174b5d5fec7eec07c5a09282
Danil-Kn <dk102801@mail.ru>
2023-08-17 10:39:55 +0300

    version 1.1

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="app">
+    <SideBar />
+    <main>
+      <RouterView></RouterView>
+    </main>
+  </div>
+</template>
+
+
+<script setup>
+import SideBar from '@components/Sidebar/SideBar.vue'
+</script>
+
+
+<style scoped>
+.app {
+    width: 100vw;
+    height: 100vh;
+    padding: 0;
+    overflow: hidden;
+    display: grid;
+    grid-template-columns: 12rem auto;
+}
+
+main{
+    overflow: hidden auto;
+}
+</style>
